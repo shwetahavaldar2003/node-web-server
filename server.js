ecea6a73da172bf27c4f446b6499b012bb95993c
shwetahavaldar2003 <110224418+shwetahavaldar2003@users.noreply.github.com>
2026-03-03 10:14:42 +0530

    simplify code

--- server.js
+++ server.js
@@ -2,61 +2,34 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-    var url = req.url;
-    
-    if (url === '/' || url === '/home') {
+    if (req.url === '/' || req.url === '/home') {
         fs.readFile('public/home.html', function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Error loading page');
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
         });
     }
-    else if (url === '/about') {
+    else if (req.url === '/about') {
         fs.readFile('public/about.html', function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Error loading page');
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
         });
     }
-    else if (url === '/contact') {
+    else if (req.url === '/contact') {
         fs.readFile('public/contact.html', function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Error loading page');
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
         });
     }
-    else if (url === '/css/style.css') {
+    else if (req.url === '/css/style.css') {
         fs.readFile('public/css/style.css', function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Error loading CSS');
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/css'});
-                res.end(data);
-            }
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.end(data);
         });
     }
     else {
         fs.readFile('public/404.html', function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                res.end('Page not found');
-            } else {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(data);
         });
     }
 });
